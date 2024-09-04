4965b71aefdb36f66ae2670d96c66a728074a90f
suahus-01 <suad.huseynli@rocksigma.com>
2024-09-04 13:05:47 +0200

    bim bim bam bam

--- labCode/routingtable_test.go
+++ labCode/routingtable_test.go
@@ -1,9 +1,12 @@
-package d7024e
+package kademlia
 
 import (
 	"fmt"
 	"testing"
 )
+
+// FIXME: This test doesn't actually test anything. There is only one assertion
+// that is included as an example.
 
 func TestRoutingTable(t *testing.T) {
 	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
@@ -18,5 +21,10 @@ func TestRoutingTable(t *testing.T) {
 	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), 20)
 	for i := range contacts {
 		fmt.Println(contacts[i].String())
+	}
+
+	// TODO: This is just an example. Make more meaningful assertions.
+	if len(contacts) != 6 {
+		t.Fatalf("Expected 6 contacts but instead got %d", len(contacts))
 	}
 }
