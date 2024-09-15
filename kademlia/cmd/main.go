8620caea4ffe7af07320ad7ef75c203cd0410349
jacomoelLTU <81552705+jacomoelLTU@users.noreply.github.com>
2024-09-15 10:02:15 +0200

    init kademlia

--- kademlia/cmd/main.go
+++ kademlia/cmd/main.go
@@ -2,42 +2,13 @@ package main
 
 import (
 	"fmt"
-	"kademlia"
-	"os"
+	//"kademlia"
+	//"os"
 )
 
 func main() {
-	//fmt.Println("Starting Kademlia network...")
 
-	// Retrieve the node's address and peer's contact address from environment variables
-	localAddr := os.Getenv("CONTAINER_IP")
-	rootAddr := os.Getenv("ROOT_ADDRESS")
-
-	if localAddr == "" || rootAddr == "" {
-		fmt.Println("NODE_ADDRESS or ROOT_ADDRESS not set in environment")
-	}
-
-	// Example network initialization for this node
-	network := kademlia.Network{
-		LocalID:   kademlia.NewRandomKademliaID(),
-		LocalAddr: localAddr,
-	}
-
-	// Start listening on the node's address
-	go kademlia.Listen("0.0.0.0", 8080)
-
-	// Create a contact for the peer node
-	if rootAddr != "" {
-		contact := kademlia.NewContact(kademlia.NewRandomKademliaID(), rootAddr)
-		network.SendPingMessage(&contact)
-
-	} else {
-		fmt.Println("ROOT_ADDRESS not set in environment")
-	}
-
-	// Send a ping to the root node
-	//contact := kademlia.Contact{Address: rootAddr}
-	//network.SendPingMessage(&contact)
+	kademlia.initKademlia()
 
 	// Keep the application running
 	select {}
--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -1,6 +1,45 @@
 package kademlia
 
+import (
+	"fmt"
+	"os"
+)
+
 type Kademlia struct {
+	network      *Network
+	routingTable *RoutingTable
+}
+
+func initKademlia() {
+
+	localAddr := os.Getenv("CONTAINER_IP")
+	rootAddr := os.Getenv("ROOT_ADDRESS")
+
+	// Create the network instance
+	networkZero := &Network{
+		LocalID:   NewRandomKademliaID(),
+		LocalAddr: localAddr,
+	}
+
+	me := NewContact(NewRandomKademliaID(), localAddr)
+	rootContact := NewContact(NewRandomKademliaID(), rootAddr)
+
+	routingTableZero := NewRoutingTable(me)
+
+	// Initialize the Kademlia instance and set the network
+	kademliaInstance := &Kademlia{
+		network:      networkZero,
+		routingTable: routingTableZero,
+	}
+
+	// Create a contact for the peer node
+	if rootAddr != "" {
+		networkZero.SendPingMessage(&rootContact)
+
+	} else {
+		fmt.Println("ROOT_ADDRESS not set in environment")
+	}
+
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
